29871fd96a62b36ecc4795642186cf21fed939f5
cuixuage <1157097502@qq.com>
2017-06-25 10:22:21 +0800

    firstweek

---
+++ func.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <grp.h>
+#include <time.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <sys/types.h>                                                              
+
