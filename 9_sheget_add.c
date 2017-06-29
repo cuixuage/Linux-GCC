9327e79bca441937d16c6b9e0501d852a84d3331
cuixuage <1157097502@qq.com>
2017-06-29 20:48:18 +0800

    secondweek

---
+++ 9_sheget_add.c
@@ -0,0 +1,55 @@
+#include "func.h"
+
+#define N 1000000
+
+void lock1(int *p)
+{
+	while(*p==0)
+	{
+	}
+	*p=0;
+}
+void unlock1(int *p)
+{
+	*p=1;
+}
+int main(int argc,char* argv[])
+{
+	int shmid;
+	shmid=shmget(1000,4096,IPC_CREAT|0600);
+	if(-1==shmid)
+	{
+		perror("shmget");
+		return -1;
+	}
+	int* p;
+	p=(int*)shmat(shmid,NULL,0);
+	if(p==(int*)-1)
+	{
+		perror("shmat");
+		return -1;
+	}
+	*p=0;
+	p[1]=1;
+	int i;
+	if(!fork())
+	{
+		for(i=0;i<N;i++)
+		{
+			lock1(&p[1]);
+			*p=*p+1;
+			unlock1(&p[1]);
+		}
+		exit(0);
+	}else{
+		for(i=0;i<N;i++)
+		{
+			lock1(&p[1]);
+			*p=*p+1;
+			unlock1(&p[1]);
+		}
+		wait(NULL);
+		printf("the value %d\n",*p);
+		return 0;
+	}
+}
--- func.h
+++ func.h
@@ -7,10 +7,14 @@
 #include <grp.h>
 #include <time.h>
 #include <pwd.h>
+#include <signal.h>
 #include <sys/stat.h>
 #include <sys/select.h>
 #include <sys/time.h>
 #include <sys/mman.h>
 #include <sys/types.h>
-#include <sys/wait.h>                                                              
+#include <sys/wait.h> 
+#include <sys/sem.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>                                                             
 
