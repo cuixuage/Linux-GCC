7ea506941843912081a5feafd66bc8c9a0592f45
cuixuage <1157097502@qq.com>
2017-07-02 11:34:00 +0800

    project1

---
+++ 进程通信深化/src/Ashow.c
@@ -0,0 +1,22 @@
+#include "func.h"
+struct Info{
+	int flag;
+	char buf[100];
+};
+int main(int argc,char* argv[])
+{
+	int shmid;// 获取共享内存
+	shmid=shmget(1000,4096,IPC_CREAT|0600);
+	struct Info* info;
+	info=(struct Info*)shmat(shmid,NULL,0);
+	memset(info,0,4096);
+	while(1){
+		if(info->flag==0) {
+			printf("%s\n",info->buf);
+			info->flag=1;
+		}
+		else if(info->flag==2) kill(getpid(),SIGKILL);
+	}
+	return 0;
+}
+
