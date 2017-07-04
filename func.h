b41463abe9ca443860ac10a708415ecd6193fbac
cuixuage <1157097502@qq.com>
2017-07-04 21:18:08 +0800

    thiredweek

--- func.h
+++ func.h
@@ -17,5 +17,10 @@
 #include <sys/sem.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>   
-#include <pthread.h>                                                          
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>                                                          
 
