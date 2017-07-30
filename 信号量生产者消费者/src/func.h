8c15f96761b1e0deb682adf45210f0aaba3cf05e
cuixuage <1157097502@qq.com>
2017-07-30 15:30:30 +0800

    delete

--- 信号量生产者消费者/src/func.h
+++
@@ -1,9 +0,0 @@
-#include <sys/sem.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
