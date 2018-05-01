99aff57c208beaa9a1923433fca3d7e418f9d308
cuixuange <1157097502@qq.com>
2018-05-01 16:44:51 +0800

    Update go_channel.go

--- Easy_Project/信号量生产者消费者/go_channel.go
+++ Easy_Project/信号量生产者消费者/go_channel.go
@@ -1,4 +1,4 @@
-var messageBuffer = make(chan int, 3)
+var messageBuffer = make(chan int, 3)   //  缓冲为3*int的大小的队列  管道
 func produce () {
  for i := 0; i < 1000; i++ {
  messageBuffer <- i
@@ -14,3 +14,6 @@ func main () {
  go produce()
  go consume()
 }
+
+
+//http://legendtkl.com/2017/07/30/understanding-golang-channel/
